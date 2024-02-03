184cb5784bed93f66a420fc6c7460c1e8d1b9140
ilya-iliks <ilya.ilik.mail@gmail.com>
2024-02-03 22:47:11 +0300

    Fix

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -3,50 +3,29 @@ import { useState } from "react";
 // import styles from "./Test.module.css";
 
 const Test = () => {
-  const [text, setText] = useState("");
   const [array, setArray] = useState([]);
-  function handleClick() {
-    const newArray = [...array, text];
-    setArray(newArray);
-    setText("");
-  }
 
   let testButton1 = () => {
+    let text = "Товар 1";
     const newArray = [...array, text];
     setArray(newArray);
-    setText("Товар 1");
   };
 
   let testButton2 = () => {
+    let text = "Товар 2";
     const newArray = [...array, text];
     setArray(newArray);
-    setText("Товар 2");
   };
-
-  let testButton3 = () => {
-    const newArray = [...array, text];
-    setArray(newArray);
-    setText("Товар 3");
-  };
-
-
-
   return (
     <div>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={() => handleClick()}>Добавить текст</button>
-      {array.map((item, index) => (
-        <div key={index}>{item}</div>
+        {array.map((item, index) => (
+        <p key={index}>{item}</p>
       ))}
+
       <button onClick={testButton1}>Товар 1</button>
       <button onClick={testButton2}>Товар 2</button>
-      <button onClick={testButton3}>Товар 3</button>
-
-     
     </div>
   );
 };
 
 export default Test;
-
-// Это тестовая компонента
